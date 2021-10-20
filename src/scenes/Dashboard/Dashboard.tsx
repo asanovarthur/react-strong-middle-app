b973483106e51821ce8f1cde86cb5007c90df23a
Arthur Asanov <asanovarthur1@gmail.com>
2021-10-20 12:36:10 +0300

    = ui wip

--- src/scenes/Dashboard/Dashboard.tsx
+++ src/scenes/Dashboard/Dashboard.tsx
@@ -1,60 +1,11 @@
-import { useCallback, useState, useEffect } from "react";
-import { useRecoilState } from "recoil";
-import userAtom from "recoil/user";
-import dataAtom from "recoil/data";
-import { db, logout } from "provider/auth";
+import styles from "./Dashboard.module.scss";
 
 export const Dashboard = () => {
-  const [user, setUser] = useRecoilState(userAtom);
-  const [recoilData, setRecoilData] = useRecoilState(dataAtom);
-  const [inputValue, setInputValue] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const saveData = useCallback(() => {
-    db.collection("data")
-      .doc(`${user.id}`)
-      .set({ data: inputValue })
-      .then(() => setRecoilData(inputValue));
-  }, [user.id, inputValue, setRecoilData]);
-
-  const exit = useCallback(() => {
-    logout();
-    window.localStorage.removeItem("userToken");
-    setUser({ ...user, isLogged: false });
-  }, [user, setUser]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    let data: any;
-
-    db.collection("data")
-      .doc(`${user.id}`)
-      .get()
-      .then((document) => (data = (document.data() as any).data))
-      .finally(() => {
-        setIsLoading(false);
-        setRecoilData(!!data ? data : "нет данных");
-      });
-  }, [user.id, setRecoilData]);
-
   return (
-    <>
-      {user.name}
-      <br />
-      {user.id}
-      <br />
-      <button onClick={exit}>выйти</button>
-      <br />
-      <br />
-      <input
-        // TODO: заюзать форму
-        onChange={(e) => setInputValue(e.target.value)}
-        value={inputValue}
-      />
-      <br />
-      <button onClick={saveData}>SAVE DATA</button>
-      {isLoading && <p>загрузка...</p>}
-      <h1>{recoilData}</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.tree}>notesTree</div>
+      <div className={styles.buttons}>actionButtons</div>
+      <div className={styles.note}>note</div>
+    </div>
   );
 };
---
+++ src/scenes/Dashboard/DashboardOld.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import userAtom from "recoil/user";
+import dataAtom from "recoil/data";
+import { db, logout } from "provider/auth";
+
+export const DashboardOld = () => {
+  const [user, setUser] = useRecoilState(userAtom);
+  const [recoilData, setRecoilData] = useRecoilState(dataAtom);
+  const [inputValue, setInputValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const saveData = useCallback(() => {
+    db.collection("data")
+      .doc(`${user.id}`)
+      .set({ data: inputValue })
+      .then(() => setRecoilData(inputValue));
+  }, [user.id, inputValue, setRecoilData]);
+
+  const exit = useCallback(() => {
+    logout();
+    window.localStorage.removeItem("userToken");
+    setUser({ ...user, isLogged: false });
+  }, [user, setUser]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    let data: any;
+
+    db.collection("data")
+      .doc(`${user.id}`)
+      .get()
+      .then((document) => (data = (document.data() as any).data))
+      .finally(() => {
+        setIsLoading(false);
+        setRecoilData(!!data ? data : "нет данных");
+      });
+  }, [user.id, setRecoilData]);
+
+  return (
+    <>
+      {user.name}
+      <br />
+      {user.id}
+      <br />
+      <button onClick={exit}>выйти</button>
+      <br />
+      <br />
+      <input
+        // TODO: заюзать форму
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+      />
+      <br />
+      <button onClick={saveData}>SAVE DATA</button>
+      {isLoading && <p>загрузка...</p>}
+      <h1>{recoilData}</h1>
+    </>
+  );
+};
