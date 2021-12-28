8e37d0af70456c833473afd363067e2f3c140341
Artur Asanov <v-aasanov@microsoft.com>
2021-12-28 05:17:55 +0300

    implemented note creation

--- src/scenes/Dashboard/components/Sidebar/components/AddNote/AddNote.tsx
+++ src/scenes/Dashboard/components/Sidebar/components/AddNote/AddNote.tsx
@@ -1,13 +1,22 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { AddNoteModal } from "./AddNoteModal";
 import styles from "./AddNote.module.scss";
 
 export const AddNote: FC = () => {
+  const [showAddNoteModal, setShowAddNoteModal] = useState(false);
+
   return (
-    <div className={styles.wrap}>
-      <FontAwesomeIcon icon={faPlusSquare} onClick={() => {}} />
-      <span className={styles.name}>Add Note</span>
-    </div>
+    <>
+      <div className={styles.wrap} onClick={() => setShowAddNoteModal(true)}>
+        <FontAwesomeIcon icon={faPlusSquare} />
+        <span className={styles.name}>Add Note</span>
+      </div>
+      <AddNoteModal
+        isOpen={showAddNoteModal}
+        setShowModal={setShowAddNoteModal}
+      />
+    </>
   );
 };
---
+++ src/scenes/Dashboard/components/Sidebar/components/AddNote/AddNoteModal.tsx
@@ -0,0 +1,59 @@
+import { FC, useCallback, useState } from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { useRecoilValue, useRecoilState } from "recoil";
+import userAtom from "recoil/user";
+import { notesAtom } from "recoil/note";
+import { ButtonWithIcon } from "components";
+import { db } from "provider/auth";
+import styles from "./AddNoteModal.module.scss";
+
+type AddNoteModalProps = {
+  isOpen: boolean;
+  setShowModal: (flag: boolean) => void;
+};
+
+export const AddNoteModal: FC<AddNoteModalProps> = ({
+  isOpen,
+  setShowModal,
+}) => {
+  const { id: userId } = useRecoilValue(userAtom);
+  const [notes, setNotes] = useRecoilState(notesAtom);
+  const [noteName, setNoteName] = useState<string>("");
+
+  const handleClose = useCallback(() => {
+    setShowModal(false);
+  }, [setShowModal]);
+
+  const handleSave = useCallback(() => {
+    // TODO: change "any"
+    const newNote: any = {
+      name: noteName,
+      uri: noteName,
+      creationDate: Date.now(),
+      userId: userId,
+    };
+
+    db.collection("notes")
+      .add(newNote)
+      .then((docRef) => setNotes([...notes, { ...newNote, id: docRef.id }]))
+      .finally(() => handleClose());
+  }, [handleClose, noteName, userId, setNotes, notes]);
+
+  return (
+    <Modal open={isOpen} onClose={handleClose}>
+      <Box className={styles.box}>
+        <h3>Add note</h3>
+        <input type="text" onChange={(e) => setNoteName(e.target.value)} />
+        <br />
+        <ButtonWithIcon
+          onClick={handleSave}
+          icon={faSave}
+          text="Save"
+          className={styles.saveBtn}
+        />
+      </Box>
+    </Modal>
+  );
+};
